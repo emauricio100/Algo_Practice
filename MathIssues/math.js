27c19cd16215e7ea596265cad6969419e2ec03c7
emauricio100 <emauricio@ucdavis.edu>
2022-09-17 15:06:43 -0700

    math objects

---
+++ MathIssues/math.js
@@ -0,0 +1,31 @@
+/*
+Oh no, our Math object was "accidently" reset. Can you re-implement some of those functions? We can assure, that only non-negative numbers are passed as arguments. So you don't have to consider things like undefined, null, NaN, negative numbers, strings and so on.
+
+Here is a list of functions, we need:
+
+Math.round()
+Math.ceil()
+Math.floor()
+*/
+
+Math.round = function(number) {
+    if(number % 1 >= .5){
+      return number + (1- (number % 1))
+    } else {
+      return number - (number % 1)
+    }
+  };
+  
+  Math.ceil = function(number) {
+    if(number % 1){
+      return number + (1-(number % 1))
+    }
+    return number
+  };
+  
+  Math.floor = function(number) {
+      if(number % 1){
+      return number - (number % 1)
+    }
+    return number
+  };
