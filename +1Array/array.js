71bb938f93f05242fb22c6287446619ed4d538fb
emauricio100 <emauricio@ucdavis.edu>
2022-09-19 22:06:30 -0700

    fixed it.... it was buggging me

--- +1Array/array.js
+++ +1Array/array.js
@@ -12,33 +12,28 @@ For example the array [2, 3, 9] equals 239, adding one would return the array [2
 [4, 3, 2, 5] would return [4, 3, 2, 6]
 */
 function upArray(arr){
-    const check1 = arr.filter((x) => x < 0)
-    const check2 = arr.filter((x) => x > 9)
-    if(check1.length > 0 || check2.length > 0) return null
-    if(arr.length === 0) return null
+    if(Math.min(...arr) < 0 || Math.max(...arr) > 9|| arr.length === 0) return null
+    
     let k = arr.length-1
+    
     const update = []
-    for(let i = 0; i < arr.length; i++){
-      if(arr[k] + 1 <= 9){
-        arr[k] = arr[k] + 1
-        return arr
-      }
+    
+    if(arr[k] <= 8){
+      arr[k] = arr[k] + 1
+      return arr
     }
+    
     while(arr[k] + 1 === 10){
       update.push(0)
       k--
       arr.pop()
     }
+    
     if(arr.length === 0){
-      update.push(1)
-      update.reverse()
+      update.unshift(1)
       return update
     }
+    
     arr[k] = arr[k] + 1
-    let i = 0
-    while( i < update.length){
-      arr.push(0)
-      i++
-    }
-    return arr
+    return arr.concat(update)
   }
