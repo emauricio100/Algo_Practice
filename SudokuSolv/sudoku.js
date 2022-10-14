abd83bf99992c6731c307bcbe741d5e2a282a01f
emauricio100 <emauricio@ucdavis.edu>
2022-10-14 14:38:40 -0700

    sudoku solver!

--- SudokuSolv/sudoku.js
+++ SudokuSolv/sudoku.js
@@ -1,42 +1,70 @@
 /*
-Write a function done_or_not/DoneOrNot passing a board (list[list_lines]) as parameter. If the board is valid return 'Finished!', otherwise return 'Try again!'
+Write a function that will solve a 9x9 Sudoku puzzle. The function will take one argument consisting of the 2D puzzle array, with the value 0 representing an unknown square.
 
-Sudoku rules:
+The Sudokus tested against your function will be "easy" (i.e. determinable; there will be no need to assume and test possibilities on unknowns) and can be solved with a brute-force approach.
 
-Complete the Sudoku puzzle so that each and every row, column, and region contains the numbers one through nine only once.
+For Sudoku rules, see the Wikipedia article.
 
+var puzzle = [
+            [5,3,0,0,7,0,0,0,0],
+            [6,0,0,1,9,5,0,0,0],
+            [0,9,8,0,0,0,0,6,0],
+            [8,0,0,0,6,0,0,0,3],
+            [4,0,0,8,0,3,0,0,1],
+            [7,0,0,0,2,0,0,0,6],
+            [0,6,0,0,0,0,2,8,0],
+            [0,0,0,4,1,9,0,0,5],
+            [0,0,0,0,8,0,0,7,9]];
+
+sudoku(puzzle);
+/* Should return
+[[5,3,4,6,7,8,9,1,2],
+[6,7,2,1,9,5,3,4,8],
+[1,9,8,3,4,2,5,6,7],
+[8,5,9,7,6,1,4,2,3],
+[4,2,6,8,5,3,7,9,1],
+[7,1,3,9,2,4,8,5,6],
+[9,6,1,5,3,7,2,8,4],
+[2,8,7,4,1,9,6,3,5],
+[3,4,5,2,8,6,1,7,9]]
 */
-
-function doneOrNot(board){
-    const three_by_three_blocks = []
-    
-    for (let i = 0; i < 9; i += 3) {
-      for (let j = 0; j < 9; j += 3) {
-        let blockList = []
-        for (let k = 0; k < 3; k++) {
-          for (let l = 0; l < 3; l++) {
-            blockList.push(board[i + k][j + l])
+function sudoku(puzzle) {
+  const isValid = (grid, row, col, number) => {
+    for(let i = 0; i < 9; i++){
+      if(grid[i][col] === number) return false
+    }
+    for(let i = 0; i < 9; i++){
+      if(grid[row][i] === number) return false
+    }
+    let miniRow = row - (row % 3)
+    let miniCol = col - (col % 3)
+    for(let i = miniRow; i < miniRow + 3; i++){
+      for(let j = miniCol; j < miniCol + 3; j++){
+        if(grid[i][j] === number) return false
+      }
+    }
+    return true
+  }
+  
+  const solve = (grid) => {
+  for(let row = 0; row < puzzle.length; row++){
+    for(let col = 0; col < puzzle.length; col++){
+      if(grid[row][col] === 0){
+        for(let guess = 1; guess < 10; guess++){
+          if(isValid(grid, row, col, guess)){
+            grid[row][col] = guess
+            if(solve(grid)){
+              return true
+            }
+            grid[row][col] = 0
           }
         }
-        three_by_three_blocks.push(blockList);
+        return false
       }
     }
-    
-    const block = three_by_three_blocks
-    
-    for(let i = 0; i < block.length; i++){
-      if(block[i].reduce((a,b) => a+b, 0) !== 45){
-        return 'Try again!'
-      }
-    }
-    
-    const colSum = board.reduce((a, b) => a.map((x, i) => x + b[i]))
-    const rowSum = board.map(r => r.reduce((a, b) => a + b))
-    
-    for(let i = 0; i < 9; i++){
-      if(colSum[i] !== 45 || rowSum[i] !== 45){
-        return 'Try again!'
-      }
-    }
-    return "Finished!"
   }
+    return true
+  }
+  solve(puzzle)
+  return puzzle
+}
