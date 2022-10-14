6045d11fb8d00cfebfe17fabca7830288c18a259
emauricio100 <emauricio@ucdavis.edu>
2022-10-14 14:57:52 -0700

    mini sudoku!

--- MiniSudoku/mini.js
+++ MiniSudoku/mini.js
@@ -1,50 +1,63 @@
+/*
+Mr Crank has assigned everyone in your class a Mini Sudoku puzzle to solve. Nobody is allowed to leave until the puzzles are completed, even though it is a Friday afternoon and the sun is shining. You decide that if you can come up with function to solve any Mini Sudoku, it would be a great way to test you coding skills and become the most popular kid in your class.
 
+Here is an example of a partially filled out mini sudoku, the zeros represent blank spaces. Each blank space must be filled in so that every column contains the numbers 1-4 and every row contains the numbers 1-4. The 2x2 blocks should also contain the numbers 1-4.
+
+                        [[0, 0, 2, 0], 
+                         [0, 3, 0, 4],
+                         [3, 0, 4, 0],
+                         [0, 2, 0, 0]],
+                         
+you must come up with a function which will correctly fill out ANY Mini Sudoku board. The test cases will only include puzzles with one possible solution or they will be impossible.
+
+if the sudoku is unsolvable the function should return "This sudoku is unsolvable!"
+*/
 
 function solve(board){
-    // your code here
-      const isItPossilbe = (x, y, n) => {
-        for(let i = 0; i < 4; i ++){
-            if(board[x][i] === n){
-              return false
-          }
-        }
-        for(let i = 0; i < 4; i++){
-          if(board[x][i] == n){
-            return false
-          }
-        }
-        const xAxis = Math.floor(x/2) * 2
-        const yAxis = Math.floor(x/2) * 2
-        for(let i = 0; i < 2; i++){
-          for(let k = 0; k < 2; k++){
-            if(board[xAxis + i][yAxis + k] === n){
-              return false
+  const isValid = (grid, row, col, number) => {
+    for(let i = 0; i < 4; i++){
+      if(grid[i][col] === number) return false
+    }
+    for(let i = 0; i < 4; i++){
+      if(grid[row][i] === number) return false
+    }
+    let miniRow = row - (row % 2)
+    let miniCol = col - (col % 2)
+    for(let i = miniRow; i < miniRow + 2; i++){
+      for(let j = miniCol; j < miniCol + 2; j++){
+        if(grid[i][j] === number) return false
+      }
+    }
+    return true
+  }
+  
+  const solver = (grid) => {
+  for(let row = 0; row < grid.length; row++){
+    for(let col = 0; col < grid.length; col++){
+      if(grid[row][col] === 0){
+        for(let guess = 1; guess < 5; guess++){
+          if(isValid(grid, row, col, guess)){
+            grid[row][col] = guess
+            if(solver(grid)){
+              return true
             }
+            grid[row][col] = 0
           }
         }
-        return true
+        return false
       }
-    const sol = () =>{
-      for(let y = 0; y < 4; y++){
-        for(let x = 0; x < 4; x++){
-          if(board[y][x] === 0){
-            for(let n = 1; n < 5; n++){
-              if(isItPossilbe(x, y, n)){
-                board[y][x] === n
-                sol()
-                board[y][x] === 0
-              }
-            }
-            return 
-          }
-        }
-      }
-      }
-    
-      console.log(sol())
-    
-      }
-      solve([[0, 0, 2, 0],
-        [0, 3, 0, 4],
-        [3, 0, 4, 0],
-        [0, 2, 0, 0]])
+    }
+  }
+    return true
+  }
+  
+  if(solver(board)){
+    for(let i =0; i < board.length; i++){
+      if(board[i].reduce((a,b) => a+b, 0) !== 10) return 'This sudoku is unsolvable!'
+    }
+    return board
+  }else{
+    return 'This sudoku is unsolvable!'
+  }
+  
+  }
